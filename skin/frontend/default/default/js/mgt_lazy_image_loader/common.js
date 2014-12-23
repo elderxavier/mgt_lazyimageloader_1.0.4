82a8ec0599e66d0da04a118ecba84791d70cb763
Elde Xavier <eldersxavier@gmail.com>
2014-12-23 17:18:20 -0200

    Inserido função para recuperar imagem orinal caso erro ou demora no "placeholder" do ."jail()" das imagens

--- skin/frontend/default/default/js/mgt_lazy_image_loader/common.js
+++ skin/frontend/default/default/js/mgt_lazy_image_loader/common.js
@@ -1,7 +1,16 @@
 $.noConflict();
-jQuery(document).ready(function($) {
-  jQuery('img.lazy').jail({
-    event: 'load+scroll',
-    placeholder : "/skin/frontend/default/default/images/mgt_lazy_image_loader/loader.gif",
-  });
+jQuery(document).ready(function() {  
 });
+jQuery(document).ready(function($) {
+  jQuery('.lazy').jail({  
+    event: 'load+scroll',
+    placeholder : "/skin/frontend/default/default/images/mgt_lazy_image_loader/loader.gif",			
+  });
+  setTimeout(function(){	
+	jQuery( ".lazy" ).each(function( index ) {		
+		longdesc = jQuery( this ).attr('long-desc');				
+		jQuery( this ).attr('src',longdesc);				
+	});
+  }, 5000);  
+});
+
